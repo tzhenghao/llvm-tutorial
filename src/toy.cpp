60cc398d87e4ba4616b0f76be84a0baec11588de
Zheng Hao Tan <hi@zhenghaotan.com>
2023-09-20 06:13:33 +0000

    clang-format

--- src/toy.cpp
+++ src/toy.cpp
@@ -1,72 +1,70 @@
 #include <string>
 
 enum Token {
-	tok_eof = -1,
-	// commands
-	tok_def = -2,
-	tok_extern = -3,
-	// primary
-	tok_identifier = -4,
-	tok_number = -5,
+  tok_eof = -1,
+  // commands
+  tok_def = -2,
+  tok_extern = -3,
+  // primary
+  tok_identifier = -4,
+  tok_number = -5,
 };
 
-static std::string IdentifierStr; // Filled in if tok_identifier 
-static double NumVal; // Filled in if tok_number
+static std::string IdentifierStr; // Filled in if tok_identifier
+static double NumVal;             // Filled in if tok_number
 
 /// gettok - returns the next token from standard input.
 static int gettok() {
-	static int LastChar = ' ';
-	// Skip whitespaces.
-	while (isspace(LastChar)) {
-		LastChar = getchar();
-	}
-}
-
-// ...
-
-if (isalpha(LastChar)) {
-	IdentifierStr = LastChar;
-	while (isalnum((LastChar = getchar())) {
-		IdentifierStr += LastChar;
-	}
-	
-	if (IdentifierStr == "def") {
-		return tok_def;
-	} else if (IdentifierStr == "extern") {
-		return tok_extern;
-	}
-	return tok_identifier;
-}
-
-// Handle numbers (values).
-if (isdigit(LastChar) || LastChar == '.') {   // Number: [0-9.]+
-  std::string NumStr;
-  do {
-    NumStr += LastChar;
+  static int LastChar = ' ';
+  // Skip whitespaces.
+  while (isspace(LastChar)) {
     LastChar = getchar();
-  } while (isdigit(LastChar) || LastChar == '.');
+  }
 
-  NumVal = strtod(NumStr.c_str(), 0);
-  return tok_number;
+  if (isalpha(LastChar)) {
+    IdentifierStr = LastChar;
+    while (isalnum((LastChar = getchar()))) {
+      IdentifierStr += LastChar;
+    }
+
+    if (IdentifierStr == "def") {
+      return tok_def;
+    } else if (IdentifierStr == "extern") {
+      return tok_extern;
+    }
+    return tok_identifier;
+  }
+
+  // Handle numbers (values).
+  if (isdigit(LastChar) || LastChar == '.') { // Number: [0-9.]+
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+
+    NumVal = strtod(NumStr.c_str(), 0);
+    return tok_number;
+  }
+
+  // Handle comments
+  if (LastChar == '#') {
+    // Comment until end of line.
+    do {
+      LastChar = getchar();
+    } while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+    if (LastChar != EOF) {
+      // just recurse and parse the next line.
+      return gettok();
+    }
+  }
+  // Check for EOF
+  if (LastChar == EOF) {
+    return tok_eof;
+  }
+
+  int ThisChar = LastChar;
+  LastChar = getchar(); // proceed to next char.
+  return ThisChar;
 }
-
-// Handle comments
-if (LastChar == '#') {
-	// Comment until end of line.
-	do {
-		LastChar = getchar();
-	} while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
-
-	if (LastChar != EOF) {
-		// just recurse and parse the next line.
-		return gettok();
-	}
-}
-// Check for EOF
-if (LastChar == EOF) {
-	return tok_eof;
-}
-
-int ThisChar = LastChar;
-LastChar = getchar(); // proceed to next char.
-return ThisChar;
