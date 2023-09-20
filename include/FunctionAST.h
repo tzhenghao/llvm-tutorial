6e59cead7bf9afa0a90c995e5fc5e1c08d055daa
Zheng Hao Tan <hi@zhenghaotan.com>
2023-09-20 20:12:23 +0000

    add function and prototype AST

---
+++ include/FunctionAST.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "ExprAST.h"
+#include "PrototypeAST.h"
+#include <memory>
+
+/// Represents the function (implementation) itself.
+class FunctionAST {
+  std::unique_ptr<PrototypeAST> Proto;
+  std::unique_ptr<ExprAST> Body;
+
+public:
+  FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+              std::unique_ptr<ExprAST> Body)
+      : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
--- include/PrototypeAST.h
+++ include/PrototypeAST.h
@@ -6,7 +6,12 @@
 /// Represents the function "prototype".
 /// Captures its name and argnames (plus # of args).
 class PrototypeAST {
-    std::string Name;
-    std::vector<std::string> Args;
-};
+  std::string Name;
+  std::vector<std::string> Args;
 
+public:
+  PrototypeAST(const std::string &Name, std::vector<std::string> Args)
+      : Name(Name), Args(std::move(Args)) {}
+
+  const std::string &getName() const { return Name; }
+};
