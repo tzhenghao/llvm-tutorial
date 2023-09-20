29d3e8c21c4af88de28680d50e798943558b4cad
Zheng Hao Tan <hi@zhenghaotan.com>
2023-09-20 07:03:18 +0000

    add pragma once and correct includes

--- include/ExprAST.h
+++ include/ExprAST.h
@@ -1,3 +1,8 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
 /// ExprAST - Base class for all expression nodes.
 class ExprAST {
 public:
@@ -12,10 +17,23 @@ public:
   NumberExprAST(double Val) : Val(Val) {}
 };
 
-/// VariableExprAST - Expression class for referencing a variable. Think "index, a" etc.
+/// VariableExprAST - Expression class for referencing a variable. Think "index,
+/// a" etc.
 class VariableExprAST : public ExprAST {
   std::string Name;
 
 public:
-  VariableExprAST(const std::string & Name) : Name(Name) {}
+  VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+/// BinaryExprAST - Expression class for referencing a variable. Think "index,
+/// a" etc.
+class BinaryExprAST : public ExprAST {
+  char Op;
+  std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+  BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
+                std::unique_ptr<ExprAST> RHS)
+      : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
 };
